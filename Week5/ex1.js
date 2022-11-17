590861727570fdd9dbbbdf16add44daf7ee082c2
jazamp <116713210+jazamp@users.noreply.github.com>
2022-11-17 09:21:37 +0700

    Create ex1.js

---
+++ Week5/ex1.js
@@ -0,0 +1,8 @@
+let items ={
+    "A4paper":120,
+    "Ruler":25,
+    "FlashDrive":130
+}
+console.log("Mr. Lee",items.A4paper*5 +items.Ruler*2)
+console.log("Mr. Yue",items.FlashDrive*2 +items.A4paper*3)
+console.log("Ms.fah ",items.FlashDrive*2 +items.A4paper*2)
